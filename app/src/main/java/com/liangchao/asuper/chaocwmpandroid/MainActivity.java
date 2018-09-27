b62eab90bc12f4c200f0e42e57e7c51d5ff83800
liangchao <superjang@163.com>
2018-09-27 22:32:19 +0800

    add alert dialog to confirm the directory exists

--- app/src/main/java/com/liangchao/asuper/chaocwmpandroid/MainActivity.java
+++ app/src/main/java/com/liangchao/asuper/chaocwmpandroid/MainActivity.java
@@ -2,7 +2,9 @@ package com.liangchao.asuper.chaocwmpandroid;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -11,8 +13,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class MainActivity extends Activity {
-    Button bt1;
+    Button bt1,bt2;
     private static final int MY_PERMISSION_REQUEST_CODE = 10000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +32,47 @@ public class MainActivity extends Activity {
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST_CODE);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final String[] items = new String[]{"创建文件","退出"};
+        builder.setIcon(R.drawable.ic_launcher_background).setTitle("未发现配置文件")
+                .setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case 0:
+                                File dir = new File("/sdcard/chaocwmp");
+                                dir.mkdir();
+                                break;
+                            case 1:
+                                finish();
+                                break;
+                        }
+                    }
+                });
+        if(!FileCheck.checkFile("/sdcard/chaocwmp/cwmpd.log")) builder.create().show();
+
         bt1 = (Button)findViewById(R.id.bt1);
+        bt2 = (Button)findViewById(R.id.bt2);
+
+
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println( NativeCwmpMethod.getStringHello());
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println( NativeCwmpMethod.getStringHello());
+                    }
+                }).start();
+
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("chaocwmp is:"+FileCheck.checkFile("/sdcard/chaocwmp"));
+                System.out.println("chaocwmp/cwmpd.log is:"+FileCheck.checkFile("/sdcard/chaocwmp/cwmpd.log"));
             }
         });
     }
