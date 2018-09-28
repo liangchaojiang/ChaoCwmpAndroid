aa35a13e8afee7e872a0558d87a00be778a12478
jiangliangchao <jiangliangchao@sunmi.com>
2018-09-28 13:41:33 +0800

    remove unused import package

--- app/src/main/java/com/liangchao/asuper/chaocwmpandroid/MainActivity.java
+++ app/src/main/java/com/liangchao/asuper/chaocwmpandroid/MainActivity.java
@@ -3,7 +3,6 @@ package com.liangchao.asuper.chaocwmpandroid;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
